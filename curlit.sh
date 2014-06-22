7f702f9927c0de3aeeaff3d4a028f7b6d030874a
mdsa <mdsa@funzio.com>
2014-06-22 03:45:26 -0700

    init

---
+++ curlit.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -d '{"username":"xyz","password":"xyz"}' http://localhost:5000/login
---
+++ server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/login', methods = ['POST'])
+def login():
+    return jsonify(request.get_json(force=True))
+
+if __name__ == "__main__":
+    app.run()
