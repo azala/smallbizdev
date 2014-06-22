e28009c7adae57cb3dc6138e73b33a3ae8e5ba49
mdsa <mdsa@funzio.com>
2014-06-22 09:50:42 -0700

    .

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,16 +1,29 @@
-$(document).ready(function(){
- $("#submit-invoice").click(function(){
-    $.ajax({
-      type:"GET"
+// $(document).ready(function(){
+//  $("#submit-invoice").click(function(){
+//     $.ajax({
+//       type:"GET"
+//     , amount: "29.99"
+//     , dataType: "application/json"
+//     , url: "http://54.84.158.190:8888/new"
+//     , desc: "tester"
+//     , success: function(data){
+//          window.location.replace(data)
+//          // console.log('you are', data)
+//       }
+//     });
+//     // window.location.replace("https://stage.wepay.com/api/preapproval/93099784/9fd8da3d")
+//   });
+// });
+
+$("#upload").on("click", function() {
+  $.ajax(type:"POST"
     , amount: "29.99"
     , dataType: "application/json"
     , url: "http://54.84.158.190:8888/new"
     , desc: "tester"
+    , time: "15"
     , success: function(data){
-         window.location.replace(data)
-         // console.log('you are', data)
+         window.location.replace(data.url)
       }
     });
-    // window.location.replace("https://stage.wepay.com/api/preapproval/93099784/9fd8da3d")
-  });
 });
